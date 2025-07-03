7371f986184ea67f51776d0d3117442773035127
raind-dev <raind0130@gmail.com>
2025-07-03 23:20:42 +0800

    Fix the wrong variable name and put some annotation in kernel function

--- Python Numba/Mask Map Generator (Simple)/main.py
+++ Python Numba/Mask Map Generator (Simple)/main.py
@@ -13,6 +13,8 @@ def mask_map(img, mask):
     if x_idx < img.shape[0] and y_idx < img.shape[1]:
         if img[x_idx][y_idx] > 0:
             mask[x_idx][y_idx] = 1
+        else:
+            mask[x_idx][y_idx] = 0 # must be set to 0, otherwise other threads are not stable to save binary mask.
 
 print("\n\n---------Start to read image: ")
 img = cv.imread(f"{os.getcwd()}/src/52.jpg")
@@ -22,18 +24,18 @@ if img is None:
     quit()
 print("Successful!")
 
-img_rgb = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-results = np.zeros_like(img_rgb)
-print(f"Image Shape: {img_rgb.shape} | Image Type: {type(img_rgb)} | Image Data Type: {img_rgb.dtype}")
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+results = np.zeros_like(img_gray)
+print(f"Image Shape: {img_gray.shape} | Image Type: {type(img_gray)} | Image Data Type: {img_gray.dtype}")
 
 print("\n\n---------Initialize GPU Resources:")
 threadsperblock = (16, 16) # according to image width and height
-blockspergrid_x = (img_rgb.shape[0] + threadsperblock[0] - 1) // threadsperblock[0] # In case the image size is not an integer multiple of the block size, enough blocks are still allocated to process the entire image.
-blockspergrid_y = (img_rgb.shape[1] + threadsperblock[1] - 1) // threadsperblock[1]
+blockspergrid_x = (img_gray.shape[0] + threadsperblock[0] - 1) // threadsperblock[0] # In case the image size is not an integer multiple of the block size, enough blocks are still allocated to process the entire image.
+blockspergrid_y = (img_gray.shape[1] + threadsperblock[1] - 1) // threadsperblock[1]
 blockspergrid = (blockspergrid_x, blockspergrid_y)
 print(f"Allocate threads: {threadsperblock} | Allocate blocks: {blockspergrid}")
 print("Copy data to device: ")
-d_img = cuda.to_device(img_rgb)
+d_img = cuda.to_device(img_gray)
 d_results = cuda.device_array_like(results)
 
 print("\n\n---------Start to generate Mask Map")
@@ -42,7 +44,7 @@ print("Getting the results from device")
 d_results.copy_to_host(results)
 
 plt.subplot(1, 2, 1)
-plt.imshow(img_rgb)
+plt.imshow(img_gray)
 plt.axis('off')
 plt.title("Original Image")
 
